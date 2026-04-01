e0a25db9c4b09cd249af92c67a174a43359adf87
Marho Aghware <marhoaghware@gmail.com>
2026-04-01 07:26:34 +0000

    fix: Expose Firebase environment variables to Next.js

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,8 +12,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const getFirebaseApp = () =>
   !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
